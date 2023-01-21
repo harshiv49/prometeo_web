f67f7a3607f42771b4724c11822c2685d4663f45
Prakhar Ojha <prakharojha12@gmail.com>
2023-01-21 11:55:30 +0530

    Added Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,16 @@ import NavigationBar from './components/navbar/Navbar';
 import Template1 from "./components/template1/Template1";
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-import Home from "./components/home/home";
 import PrivateRoute from './components/PrivateRoutes';
 import Home from './components/home/home';
+import HomePage from './components/HomePage/HomePage';
 function App() {
   return (
     <Routes>
       {/* <PrivateRoute exact path="/" component={} /> */}
          <Route path="/" element={<NavigationBar></NavigationBar>}>
-         <Route index={true} element={<Home/>} />
+          <Route index={true} element={<HomePage />} />
+         <Route path="Resumes" element={<Home/>} />
          <Route path='temp2' element={<Template1></Template1>} />
          <Route path='temp1' element={<Template></Template>} />
          <Route path="SignUp" element={<SignUp />} />
